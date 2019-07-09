e46620a3faba23cfd5d9057773af2256991142ad
samlutz <sammlutz@gmail.com>
2019-07-09 18:45:44 -0400

    code revisit and refactor

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,12 +1,13 @@
 // var for our players
-var topics = ["Lionel Messi", "Cristiano Ronaldo", "Zlatan Ibrahimovic", "Miguel Almiron", "Pele", "Diego Maragona", "Neymar"];
+const topics = ["Lionel Messi", "Cristiano Ronaldo", "Zlatan Ibrahimovic", "Miguel Almiron", "Pele", "Diego Maragona", "Neymar"];
+
 // function to render buttons
-function renderButtons() {
+ renderButtons = () => {
     // empty div before adding new buttons
     $("#giphy-buttons").empty();
     // for loop to creat buttons for our topics
     for (i = 0; i < topics.length; i++) {
-        var newButton = $("<button>");
+        const newButton = $("<button>");
         newButton.addClass("player-button");
         newButton.attr("data-name", topics[i]);
         newButton.text(topics[i]);
@@ -27,10 +28,10 @@ function renderButtons() {
 renderButtons()
 
 // click function to add new buttons to the topics array
-$(".btn-primary").on("click", function (event) {
+$(".btn-primary").on("click", event => {
     event.preventDefault();
     // grab the input from textbox
-    var newPlayerButton = $("#inlineFormInputName2").val().trim();
+    const newPlayerButton = $("#inlineFormInputName2").val().trim();
     // adding new player button to topics array
     topics.push(newPlayerButton);
     renderButtons();
@@ -41,9 +42,9 @@ $(document).on("click", ".player-button", function (event) {
 
     event.preventDefault();
     // var to store data name attr for clicked player button
-    var player = $(this).attr("data-name");
+    const player = $(this).attr("data-name");
     // query url for ajax call
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=ctCvEkxbAmDzRZG7zRbcjTkZQ39p0Rva&q=" + player + "&limit=25&offset=0&rating=G&lang=en"
+    const queryURL = "https://api.giphy.com/v1/gifs/search?api_key=ctCvEkxbAmDzRZG7zRbcjTkZQ39p0Rva&q=" + player + "&limit=25&offset=0&rating=G&lang=en"
 
     $.ajax({
         url: queryURL,
@@ -53,7 +54,7 @@ $(document).on("click", ".player-button", function (event) {
         // loop to display 10 GIFs each time a player button is clicked
         for (i = 0; i < 10; i++) {
             // store the gif path in a variable with an image tag
-            var stillGIF = $("<img>").attr("src", response.data[i].images.fixed_height_still.url);
+            const stillGIF = $("<img>").attr("src", response.data[i].images.fixed_height_still.url);
             // give stillGIF a class of "gif"
             stillGIF.addClass("gif");
             // add attributes to stillGIF
@@ -63,7 +64,7 @@ $(document).on("click", ".player-button", function (event) {
 
             stillGIF.attr("data-still", response.data[i].images.fixed_height_still.url)
             // add p tag to hold our rating
-            var gifRating = $("<p>").text("Rating: " + response.data[i].rating)
+            const gifRating = $("<p>").text("Rating: " + response.data[i].rating)
             // append gif and rating to gifdiv 
             $(".gif-div").append(stillGIF, gifRating);
             // append the still gif and rating to the giphy div
@@ -71,11 +72,12 @@ $(document).on("click", ".player-button", function (event) {
         }
     });
 })
+
 // click function to animate GIFs
 $(document.body).on("click", ".gif", function (event) {
     event.preventDefault();
 
-    var state = $(this).attr("data-state");
+    const state = $(this).attr("data-state");
 
     if (state === "still") {
         $(this).attr("src", $(this).attr("data-animate"));
@@ -85,4 +87,5 @@ $(document.body).on("click", ".gif", function (event) {
         $(this).attr("data-state", "still");
     }
 });
+
 
